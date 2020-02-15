3b44f736328b15adf47f28723efe002f5d284377
tracyzg1818 <49750050+tracyzg1818@users.noreply.github.com>
2020-02-14 22:16:52 -0500

    Add files via upload

---
+++ crawler/storage.py
@@ -0,0 +1,19 @@
+import pymysql
+
+with open('names', 'r') as file:
+    names = eval(file.read())
+config = {
+    "host": "127.0.0.1",
+    "user": "root",
+    "port": 3306,
+    "password": "123456",
+    "db": 'zhihu_santi',
+    "charset": 'utf8'
+}
+insert = "insert into name (`name`,`status`) VALUES( '%s' ,'0');"
+SQL = ""
+for name in names:
+    SQL += insert % name
+print(SQL)
+with pymysql.connect(**config) as cursor:
+    cursor.execute(SQL)
